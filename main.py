1cb8847dd4047132dae5a5158542924f92e250c8
Nereo Ontiveros <nerontiveros@gmail.com>
2021-07-31 19:10:39 +0100

    Add next_question method

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for dictionary in question_data:
     question = Question(dictionary['text'], dictionary['answer'])
     question_bank.append(question)
+
+quiz_brain = QuizBrain(question_bank)
+next_question = quiz_brain.next_question()
+
+input(f"{next_question}. (True/False):")
